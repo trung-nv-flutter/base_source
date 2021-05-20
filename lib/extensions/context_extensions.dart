20912fe63ca91c5ac70f3742436402bb57a23268
trung.nv <trung.nv@briswell-vn.com>
2021-05-20 13:37:55 +0700

    no message

--- lib/extensions/context_extensions.dart
+++ lib/extensions/context_extensions.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:permission_handler/permission_handler.dart';
 import './string_extensions.dart';
 import '../common/utils.dart';
 
-typedef PermissionCallback = void Function(
-    Permission permission, bool isGranted, bool isFirstTime);
+// typedef PermissionCallback = void Function(
+//     Permission permission, bool isGranted, bool isFirstTime);
 
 extension BuidContextExtensions on BuildContext {
   Size get size {
@@ -17,37 +17,36 @@ extension BuidContextExtensions on BuildContext {
     Navigator.of(this).pop();
   }
 
-  Future<bool> requestPermission(List<Permission> permissions) async {
-    bool isAllGranted = true;
-    Map<Permission, PermissionStatus> statuses = await permissions.request();
-    statuses.forEach((key, value) {
-      if (statuses[key] != PermissionStatus.granted) {
-        isAllGranted = false;
-      }
-    });
-    return isAllGranted;
-  }
+  // Future<bool> requestPermission(List<Permission> permissions) async {
+  //   bool isAllGranted = true;
+  //   Map<Permission, PermissionStatus> statuses = await permissions.request();
+  //   statuses.forEach((key, value) {
+  //     if (statuses[key] != PermissionStatus.granted) {
+  //       isAllGranted = false;
+  //     }
+  //   });
+  //   return isAllGranted;
+  // }
 
-  void checkAndRequestPermission(
-      List<Permission> permissions, PermissionCallback callback) async {
-    
-    int count = 0;
-    for (var i = 0; i < permissions.length; i++) {
-      final permission = permissions[i];
-      var status = await permissions[i].status;
-      if (status.isGranted) {
-        count++;
-        if (count == permissions.length) {
-          callback(permission, true, false);
-          break;
-        }
-      } else if (status.isUndetermined || status.isDenied) {
-        var result = await requestPermission(permissions);
-        callback(permission, result, status.isUndetermined);
-        break;
-      } else {}
-    }
-  }
+  // void checkAndRequestPermission(
+  //     List<Permission> permissions, PermissionCallback callback) async {
+  //   int count = 0;
+  //   for (var i = 0; i < permissions.length; i++) {
+  //     final permission = permissions[i];
+  //     var status = await permissions[i].status;
+  //     if (status.isGranted) {
+  //       count++;
+  //       if (count == permissions.length) {
+  //         callback(permission, true, false);
+  //         break;
+  //       }
+  //     } else if (status.isUndetermined || status.isDenied) {
+  //       var result = await requestPermission(permissions);
+  //       callback(permission, result, status.isUndetermined);
+  //       break;
+  //     } else {}
+  //   }
+  // }
 
   alert(
       {String title,
@@ -103,4 +102,3 @@ extension BuidContextExtensions on BuildContext {
     showDialog(context: this, builder: widgetBuilder);
   }
 }
-
